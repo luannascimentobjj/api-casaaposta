1f43820ac632d80adc5c4bcf3817e35ab5437f29
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-02-24 05:35:43 -0300

    Inclusão do sequilize

--- src/Conf/db.js
+++ src/Conf/db.js
@@ -3,11 +3,22 @@ async function connect(){
     if(global.connection && global.connection.state !== 'disconnected')
             return global.connection;
     
-            const mysql = require('mysql2/promise');
-    const connection = await mysql.createConnection("mysql://root:Xp3rtsBD2022@191.252.59.151:80/aposta");
-    console.log("Conectou no Mysql");
-    global.connection = connection;
-    return connection;
+    const { Sequelize } = require('sequelize');
+    const sequelize = new Sequelize("mysql://root:Xp3rtsBD2022@191.252.59.151:80/aposta");
+    global.connection = sequelize;
+
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+
+    //const mysql = require('mysql2/promise');
+    //const connection = await mysql.createConnection("mysql://root:Xp3rtsBD2022@191.252.59.151:80/aposta");
+    //console.log("Conectou no Mysql");
+    //global.connection = connection;
+    //return connection;
 }
 
 connect();
