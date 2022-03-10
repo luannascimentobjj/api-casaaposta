053cb226079820d1c1726bc961e7dd8dc755a716
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-03-10 06:50:27 -0300

    Inclusão da configuração de perfis, feito.

--- src/main/java/br/casaaposta/main/config/ApiApostaDbConfig.java
+++ src/main/java/br/casaaposta/main/config/ApiApostaDbConfig.java
@@ -1,49 +1,81 @@
 package br.casaaposta.main.config;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.Properties;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import br.casaaposta.main.repository.api.MetodoRespository;
 
 @Configuration
-@EnableJpaRepositories(basePackageClasses = MetodoRespository.class, entityManagerFactoryRef = "apiEntityManager")
+@EnableJpaRepositories(basePackageClasses = MetodoRespository.class, entityManagerFactoryRef = "apiApostaEntityManager")
 public class ApiApostaDbConfig {
 
-	@Bean
+	@Autowired
+	
+	
+	
 	@Primary
-	@ConfigurationProperties(prefix = "api.datasource")
+	@Profile("dev")
+	@Bean
 	DataSource apiApostaDataSource() {
 
-		
-		return DataSourceBuilder.create().build();
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		return DataSourceBuilder
+				.create()
+				.username("root")
+				.password("1234")
+				.url("jdbc:mysql://localhost/api_aposta")
+				.driverClassName("com.mysql.jdbc.Driver")
+				.build();
 
 	}
 
-	@Bean
-	@Primary
-	public LocalContainerEntityManagerFactoryBean apiEntityManager(EntityManagerFactoryBuilder builder,
-			@Qualifier("apiApostaDataSource") DataSource dataSource) {
-		Map<String, Object> properties = new HashMap<String, Object>();
-		properties.put("spring.datasource.driverClassName", "com.mysql.jdbc.Driver");
-		properties.put("spring.jpa.database-platform", "org.hibernate.dialect.MySQL8Dialect");
-		properties.put("spring.jpa.show-sql", "true");
-		properties.put("spring.jpa.hibernate.ddl-auto", "true");
-		properties.put("spring.jpa.defer-datasource-initialization", "true");
+
+		@Bean
+		@Primary
+        public LocalContainerEntityManagerFactoryBean apiApostaEntityManager() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(apiApostaDataSource());
+        em.setPackagesToScan(new String[] { "br.casaaposta.main.entity.api" });
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.ddl-auto", "update");
+        properties.setProperty("defer-datasource-initialization", "true");
+        properties.setProperty("show-sql", "true");
+        properties.setProperty("hibernate.dialect","org.hibernate.dialect.MySQL8Dialect");
+        em.setJpaProperties(properties);
+        
+        return em;
+    }
 	
-		return builder.dataSource(dataSource)
-				.packages("br.casaaposta.main.entity.api").properties(properties).build();
 
+
+	@Primary
+    @Bean
+    public PlatformTransactionManager apiTransactionManager() {
+ 
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(apiApostaEntityManager().getObject());
+        return transactionManager;
+    }
+	
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf){
+		return new JpaTransactionManager(emf);
+		
 	}
-
 }
+
--- src/main/java/br/casaaposta/main/config/ApostaDbConfig.java
+++ src/main/java/br/casaaposta/main/config/ApostaDbConfig.java
@@ -8,17 +8,19 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import br.casaaposta.main.repository.consumer.LigaRepository;
 
 @Configuration
+@PropertySource({"classpath:application.properties"})
 @EnableJpaRepositories(basePackageClasses = LigaRepository.class,  entityManagerFactoryRef = "consumerEntityManager")
 public class ApostaDbConfig {
 
 	@Bean
-	@ConfigurationProperties(prefix = "consumer.datasource")
+	@ConfigurationProperties(prefix = "spring.second-datasource")
 	DataSource consumerDataSource() {
 
 		return DataSourceBuilder.create().build();
@@ -31,5 +33,7 @@ public class ApostaDbConfig {
 		return builder.dataSource(dataSource).packages("br.casaaposta.main.entity.consumer").build();
 
 	}
+	
+	
 
 }
