d7bf34cb472be7debda819b872756e9efc3ce1c1
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-03-26 20:41:59 -0300

    Inclusão do authorize para passar o token

--- src/main/java/br/casaaposta/main/config/SwaggerConfig.java
+++ src/main/java/br/casaaposta/main/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package br.casaaposta.main.config;
 
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,43 +12,52 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig implements WebMvcConfigurer{
-	
-	
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+public class SwaggerConfig implements WebMvcConfigurer {
 
-        registry
-                .addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+	public static final String AUTHORIZATION_HEADER = "Authorization";
 
-        registry
-                .addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-	
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-          .select()
-          .apis(RequestHandlerSelectors.basePackage("br.casaaposta.main.controllers"))
-          .paths(PathSelectors.any())
-          .build()
-          .apiInfo(getApiInfo());
-    }
-    
-    private ApiInfo getApiInfo() {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        return new ApiInfoBuilder()
-                .title("Swagger API Doc")
-                .description("Api Xperts")
-                .version("1.0.0")
-                .build();
-    }
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Arrays.asList(apiKey())).select()
+				.apis(RequestHandlerSelectors.basePackage("br.casaaposta.main.controllers")).paths(PathSelectors.any())
+				.build().apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+
+		return new ApiInfoBuilder().title("Swagger API Doc").description("Api Xperts").version("1.0.0").build();
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+	}
 }
