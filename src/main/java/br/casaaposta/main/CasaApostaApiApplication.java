84c2a9eff54e583cf0a5719a030d073245380527
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-04-01 11:17:40 -0300

    Criação do método de criptografia de senhas.

--- src/main/java/br/casaaposta/main/CasaApostaApiApplication.java
+++ src/main/java/br/casaaposta/main/CasaApostaApiApplication.java
@@ -2,16 +2,21 @@ package br.casaaposta.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CasaApostaApiApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasaApostaApiApplication.class, args);
 	}
+
 
 
 }
--- src/main/java/br/casaaposta/main/config/WebSecurityConfig.java
+++ src/main/java/br/casaaposta/main/config/WebSecurityConfig.java
@@ -1,62 +1,25 @@
 package br.casaaposta.main.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import br.casaaposta.main.filter.JwtRequestFilter;
-import br.casaaposta.main.model.JwtAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-	@Autowired
-	private UserDetailsService jwtUserDetailsService;
-
-	@Autowired
-	private JwtRequestFilter jwtRequestFilter;
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-	}
-
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
+	
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
+	public PasswordEncoder getPasswordEncoder() {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+		
 	}
-
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
-				// Não cheque essas requisições
-				.authorizeRequests().antMatchers("/authenticate", "/v2/api-docs", "/configuration/ui",
-						"/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**")
-				.permitAll().
-				// Qualquer outra requisição deve ser checada
-				anyRequest().authenticated().and().exceptionHandling()
-				.authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-	}
+	
 }
