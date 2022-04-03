384e54afd13a73af8b8e5abeedfffa189dd613c5
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-04-03 08:15:53 -0300

    OddsEuroCup Data utilização.

--- src/main/java/br/casaaposta/main/controllers/OddsEuroCupController.java
+++ src/main/java/br/casaaposta/main/controllers/OddsEuroCupController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.casaaposta.main.data.OddsEuroCupData;
 import br.casaaposta.main.dto.OddsDTO;
 import br.casaaposta.main.entity.consumer.OddsEuroCup;
-import br.casaaposta.main.repository.consumer.OddsEuroCupRepository;
 import io.swagger.annotations.Api;
 
 @RestController
@@ -21,13 +21,13 @@ import io.swagger.annotations.Api;
 public class OddsEuroCupController {
 
 	@Autowired
-	private OddsEuroCupRepository oddsEuroCupRepository_;
-	
+	private OddsEuroCupData oddsEuroCupData_;
+		
 	
 	@GetMapping(value = "findOdds/")
 	public ResponseEntity<List<OddsDTO>> findOddsEuroCup() {
 
-		List<OddsEuroCup> resultados = oddsEuroCupRepository_.findAll();
+		List<OddsEuroCup> resultados = oddsEuroCupData_.findAll();
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(resultados), HttpStatus.OK);
 
@@ -36,7 +36,7 @@ public class OddsEuroCupController {
 	
 	@GetMapping(value = "findByDate/{date}")
 	public ResponseEntity<List<OddsDTO>> findResultsByData(@PathVariable String date) {
-		List<OddsEuroCup> results = oddsEuroCupRepository_.findByDataOrderByResultadoTipoAsc(date.replace("-", "/"));
+		List<OddsEuroCup> results = oddsEuroCupData_.findByDataOrderByResultadoTipoAsc(date.replace("-", "/"));
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(results), HttpStatus.OK);
 
@@ -44,7 +44,7 @@ public class OddsEuroCupController {
 	
 	@GetMapping(value = "findByType/{type}")
 	public ResponseEntity<List<OddsDTO>> findResultsByType(@PathVariable String type) {
-		List<OddsEuroCup> results = oddsEuroCupRepository_.findByResultadoTipoOrderByResultadoTipoAsc(type);
+		List<OddsEuroCup> results = oddsEuroCupData_.findByResultadoTipoOrderByResultadoTipoAsc(type);
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(results), HttpStatus.OK);
 
@@ -52,7 +52,7 @@ public class OddsEuroCupController {
 	
 	@GetMapping(value = "findResultsByTimeCasa/{timeCasa}")
 	public ResponseEntity<List<OddsDTO>> findResultsByTimeCasa(@PathVariable String timeCasa) {
-		List<OddsEuroCup> results = oddsEuroCupRepository_.findByTimeCasaOrderByResultadoTipoAsc(timeCasa);
+		List<OddsEuroCup> results = oddsEuroCupData_.findByTimeCasaOrderByResultadoTipoAsc(timeCasa);
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(results), HttpStatus.OK);
 
@@ -60,7 +60,7 @@ public class OddsEuroCupController {
 	
 	@GetMapping(value = "findResultsByTimeVisitante/{timeVisitante}")
 	public ResponseEntity<List<OddsDTO>> findResultsByTimeVisitante(@PathVariable String timeVisitante) {
-		List<OddsEuroCup> results = oddsEuroCupRepository_.findByTimeVisitanteOrderByResultadoTipoAsc(timeVisitante);
+		List<OddsEuroCup> results = oddsEuroCupData_.findByTimeVisitanteOrderByResultadoTipoAsc(timeVisitante);
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(results), HttpStatus.OK);
 
@@ -68,7 +68,7 @@ public class OddsEuroCupController {
 	
 	@GetMapping(value = "findResultsByHora/{hora}")
 	public ResponseEntity<List<OddsDTO>> findResultsByHora(@PathVariable String hora) {
-		List<OddsEuroCup> results = oddsEuroCupRepository_.findByHoraOrderByResultadoTipoAsc(hora);
+		List<OddsEuroCup> results = oddsEuroCupData_.findByHoraOrderByResultadoTipoAsc(hora);
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(results), HttpStatus.OK);
 
@@ -76,7 +76,7 @@ public class OddsEuroCupController {
 	
 	@GetMapping(value = "findJogo/{timeCasa}/{timeVisitante}")
 	public ResponseEntity<List<OddsDTO>> findJogo(@PathVariable String timeCasa, String timeVisitante) {
-		List<OddsEuroCup> results = oddsEuroCupRepository_.findByTimeCasaAndTimeVisitanteOrderByResultadoTipoAsc(timeCasa, timeVisitante);
+		List<OddsEuroCup> results = oddsEuroCupData_.findByTimeCasaAndTimeVisitanteOrderByResultadoTipoAsc(timeCasa, timeVisitante);
 
 		return new ResponseEntity<>(OddsDTO.converterToEuroCup(results), HttpStatus.OK);
 
--- src/main/java/br/casaaposta/main/data/OddsEuroCupData.java
+++ src/main/java/br/casaaposta/main/data/OddsEuroCupData.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Component;
 import br.casaaposta.main.entity.consumer.OddsEuroCup;
 import br.casaaposta.main.interfaces.OddsEuroCupDataInterface;
 import br.casaaposta.main.repository.consumer.OddsEuroCupRepository;
+
 @Component
-public class OddsEuroCupData implements OddsEuroCupDataInterface{
+public class OddsEuroCupData implements OddsEuroCupDataInterface {
 
 	@Autowired
 	private OddsEuroCupRepository oddsEuroCupRepository_;
-	
+
+	@Deprecated
 	@Override
 	public OddsEuroCup findByTollTipAndMinutoAndHoraAndResultadoTipo(String toolTip, int minuto, int hora,
 			String resultadoTipo) {
@@ -22,53 +24,97 @@ public class OddsEuroCupData implements OddsEuroCupDataInterface{
 	}
 
 	@Override
-	public List<OddsEuroCup> findByDataOrderByResultadoTipoAsc(String data) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<OddsEuroCup> findByDataOrderByResultadoTipoAsc(String date) {
+
+		try {
+
+			return oddsEuroCupRepository_.findByDataOrderByResultadoTipoAsc(date.replace("-", "/"));
+
+		} catch (Exception e) {
+
+			return null;
+		}
+
 	}
 
 	@Override
-	public List<OddsEuroCup> findByResultadoTipoOrderByResultadoTipoAsc(String resultadoTipo) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<OddsEuroCup> findByResultadoTipoOrderByResultadoTipoAsc(String type) {
+
+		try {
+
+			return oddsEuroCupRepository_.findByResultadoTipoOrderByResultadoTipoAsc(type);
+
+		} catch (Exception e) {
+
+			return null;
+		}
+
 	}
 
 	@Override
 	public List<OddsEuroCup> findByTimeVisitanteOrderByResultadoTipoAsc(String timeVisitante) {
-		// TODO Auto-generated method stub
-		return null;
+
+		try {
+
+			return oddsEuroCupRepository_.findByTimeVisitanteOrderByResultadoTipoAsc(timeVisitante);
+
+		} catch (Exception e) {
+
+			return null;
+		}
+
 	}
 
 	@Override
-	public List<OddsEuroCup> findByTimeCasaOrderByResultadoTipoAsc(String timeVisitante) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<OddsEuroCup> findByTimeCasaOrderByResultadoTipoAsc(String timeCasa) {
+		try {
+			return oddsEuroCupRepository_.findByTimeCasaOrderByResultadoTipoAsc(timeCasa);
+
+		} catch (Exception e) {
+
+			return null;
+		}
+
 	}
 
 	@Override
 	public List<OddsEuroCup> findByHoraOrderByResultadoTipoAsc(String hora) {
-		// TODO Auto-generated method stub
-		return null;
+
+		try {
+			return oddsEuroCupRepository_.findByHoraOrderByResultadoTipoAsc(hora);
+
+		} catch (Exception e) {
+			return null;
+		}
+
 	}
 
 	@Override
 	public List<OddsEuroCup> findByTimeCasaAndTimeVisitanteOrderByResultadoTipoAsc(String timeCasa,
 			String timeVisitante) {
-		// TODO Auto-generated method stub
-		return null;
+
+		try {
+
+			return oddsEuroCupRepository_.findByTimeCasaAndTimeVisitanteOrderByResultadoTipoAsc(timeCasa,
+					timeVisitante);
+
+		} catch (Exception e) {
+
+			return null;
+		}
+
 	}
 
 	@Override
 	public List<OddsEuroCup> findAll() {
-		
+
 		try {
 			return oddsEuroCupRepository_.findAll();
 		} catch (Exception e) {
-			
+
 			return null;
 		}
-		
-		
+
 	}
 
 }
