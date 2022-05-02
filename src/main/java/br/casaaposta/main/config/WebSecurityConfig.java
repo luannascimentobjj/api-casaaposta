be748c963f0db7b5d0d94a54690f05250ed423ef
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-05-02 09:34:47 -0300

    Revert "configuração de cors."
    This reverts commit 204bccc24ac8925277653ab12a16ee5d63e298d7.

--- src/main/java/br/casaaposta/main/config/WebSecurityConfig.java
+++ src/main/java/br/casaaposta/main/config/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package br.casaaposta.main.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +12,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.casaaposta.main.filter.JwtRequestFilter;
 
@@ -74,5 +69,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 		httpSecurity.cors();
 	}
-	
 }
