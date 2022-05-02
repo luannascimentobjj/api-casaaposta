9402fb814992e5001de23e0d0e762c54581e6585
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-05-02 18:50:16 -0300

    Ajuste do cors

--- src/main/java/br/casaaposta/main/config/WebSecurityConfig.java
+++ src/main/java/br/casaaposta/main/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package br.casaaposta.main.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.casaaposta.main.filter.JwtRequestFilter;
 
@@ -43,10 +48,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+		
+	}
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
+		httpSecurity.cors().and().csrf().disable()
 				// Não cheque essas requisições
 				.authorizeRequests().antMatchers("/authenticate", "/v2/api-docs", "/configuration/ui",
 						"/swagger-resources/**", "/configuration/**", "/swagger-ui.html", "/webjars/**")
