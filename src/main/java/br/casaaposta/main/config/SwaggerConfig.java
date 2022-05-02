30bad7dbc51087ead828aa0af94d776f3bb573ff
Luan Nascimento Oliveira <nascimento.hate@gmail.com>
2022-05-02 10:05:04 -0300

    REVERT, RETIRANDO O CORS.

--- src/main/java/br/casaaposta/main/config/SwaggerConfig.java
+++ src/main/java/br/casaaposta/main/config/SwaggerConfig.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
